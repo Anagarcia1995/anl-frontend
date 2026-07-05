7bceb41c5d34042c35787ed9ab7eeca611f4f86d
Ana Garcia <anagahi1212@gmail.com>
2026-07-05 22:23:08 +0200

    add releases.js

---
+++ src/data/releases.js
@@ -0,0 +1,75 @@
+export const releases = [
+{
+  id: 1,
+  title: "Music From The Future",
+  artist: "Art No Logia",
+  cover: "/cover/c1.webp",
+
+  spotify: "#",
+  appleMusic: "#",
+  soundcloud: "#",
+
+  releaseDate: "14 June 2026",
+  label: "COCOA MUSIC"
+},
+  {
+    id: 2,
+    title: "Midnight Groove",
+    artist: "Art No Logia",
+    cover: "/cover/c2.webp",
+      spotify: "#",
+  appleMusic: "#",
+  soundcloud: "#",
+
+  releaseDate: "14 June 2026",
+  label: "COCOA MUSIC"
+  },
+  {
+    id: 3,
+    title: "Retro Vision",
+    artist: "Art No Logia",
+    cover: "/cover/c3.webp",
+      spotify: "#",
+  appleMusic: "#",
+  soundcloud: "#",
+
+  releaseDate: "14 June 2026",
+  label: "COCOA MUSIC"
+  },
+  {
+    id: 4,
+    title: "Break The Silence",
+    artist: "Art No Logia",
+    cover: "/cover/c4.webp",
+      spotify: "#",
+  appleMusic: "#",
+  soundcloud: "#",
+
+  releaseDate: "14 June 2026",
+  label: "COCOA MUSIC"
+  },
+  {
+    id: 5,
+    title: "Late Hours",
+    artist: "Art No Logia",
+    cover: "/cover/c5.webp",
+      spotify: "#",
+  appleMusic: "#",
+  soundcloud: "#",
+
+  releaseDate: "14 June 2026",
+  label: "COCOA MUSIC"
+  },
+  {
+    id: 6,
+    title: "Underground Motion",
+    artist: "Art No Logia",
+    cover: "/cover/c6.webp",
+      spotify: "#",
+  appleMusic: "#",
+  soundcloud: "#",
+
+  releaseDate: "14 June 2026",
+  label: "COCOA MUSIC"
+  },
+]
