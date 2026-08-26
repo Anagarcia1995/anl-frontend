922b2b08072f791f0626e1f2e29f99ba8c4f25ab
Ana Garcia <anagahi1212@gmail.com>
2026-08-26 21:05:35 +0200

    refine responsive UI and release layouts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 import { Box } from "@chakra-ui/react"
 
 import ReleaseSection from "../components/Home/Release/ReleaseSection"
@@ -7,12 +9,49 @@ import HeroSection from "../components/Home/HeroSection"
 
 export default function HomePage() {
 
+  const [showOverlay, setShowOverlay] = useState(true)
+  const [fadeOverlay, setFadeOverlay] = useState(false)
+
+  useEffect(() => {
+
+    const fadeTimer = setTimeout(() => {
+      setFadeOverlay(true)
+    }, 600)
+
+    const removeTimer = setTimeout(() => {
+      setShowOverlay(false)
+    }, 1100)
+
+    return () => {
+      clearTimeout(fadeTimer)
+      clearTimeout(removeTimer)
+    }
+
+  }, [])
+
   return (
-    <Box>
+    <Box position="relative">
+
       <HeroSection />
+
       <ReleaseSection />
+
       <TourSection />
+
       <VideosSection />
+
+      {showOverlay && (
+        <Box
+          position="fixed"
+          inset="0"
+          zIndex="9999"
+          bg="black"
+          opacity={fadeOverlay ? 0 : 1}
+          transition="opacity .5s ease"
+          pointerEvents="none"
+        />
+      )}
+
     </Box>
   )
 }
